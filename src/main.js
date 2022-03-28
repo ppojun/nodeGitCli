8a603d514198835a541ac15c7d29800ddd764f6a
ppojun <120106@naver.com>
2022-03-28 13:30:18 +0900

    Build basic Cli commands

--- src/main.js
+++ src/main.js
@@ -24,12 +24,23 @@ program
   .command('list-bugs')
   .description('List issues with bug label')
   .action(async () => {
-    // console.log('Before readfile...')
-    // const result = await fs.promises.readFile('.gitignore')
-    // console.log('readfile result: ', result)
-    console.log('List bugs')
-  })
+    const result = await octokit.rest.issues.listForRepo({
+      owner: 'ppojun',
+      repo: 'nodegitcli',
+      labels: 'bug',
+    })
 
+    const issuesWithBugLabel = result.data.filter(
+      (issue) =>
+        issue.labels.find((label) => label.name === 'bug') !== undefined
+    )
+    const output = issuesWithBugLabel.map((issue) => ({
+      title: issue.title,
+      number: issue.number,
+    }))
+    console.log(output)
+  })
+// 풀 리퀘스트를 모두 검사 후 만약 너무 diff가 큰(100줄) 풀 리퀘스트가 있으면 'too-big'이라는 레이블을 붙인다.
 program
   .command('check-prs')
   .description('Check pull request status')
